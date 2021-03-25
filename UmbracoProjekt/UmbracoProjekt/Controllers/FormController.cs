dc27c9eb6daae97aee12ed69c232ef4c839b2e7c
Zeegaan <70372949+Zeegaan@users.noreply.github.com>
2021-03-25 19:36:14 +0100

    Form created
    Form page created, with the 4 fields from the person class. Create() Action created in FormController, just returns view for now.

--- UmbracoProjekt/UmbracoProjekt/Controllers/FormController.cs
+++ UmbracoProjekt/UmbracoProjekt/Controllers/FormController.cs
@@ -3,12 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UmbracoProjekt.Models;
 
 namespace UmbracoProjekt.Controllers
 {
     public class FormController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Person p)
         {
             return View();
         }
