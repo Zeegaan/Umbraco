ab3bb3ea03ba929eed72cfc3bf57e0467e007c3d
Zeegaan <70372949+Zeegaan@users.noreply.github.com>
2021-04-07 13:04:54 +0200

    Unit tests implemented

--- UmbracoProjekt/UnitTestProject/UnitTests.cs
+++ UmbracoProjekt/UnitTestProject/UnitTests.cs
@@ -11,18 +11,13 @@ using UmbracoProjekt.Models;
 namespace UnitTestProject
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTests
     {
-        private SerialNumberRepo numbers = new SerialNumberRepo();
-        private Mock<DbSet<Form>> mockSet = new Mock<DbSet<Form>>();
-        private Mock<FormDataContext> mockContext = new Mock<FormDataContext>();
-        FormController controller;
         Form a, b, c;
         [TestInitialize]
         public void Init()
         {
             //Filling our objects with data, so we can use them to test the database
-            mockContext.Setup(m => m.Forms).Returns(mockSet.Object);
             a = new Form
             {
                 EmailAdress = "n@g.com",
@@ -44,28 +39,16 @@ namespace UnitTestProject
                 LastName = "Geisle",
                 SerialNumber = "1"
             };
-            controller = new FormController(mockContext.Object, numbers);
         }
         [TestMethod]
         public void TestAddTwice()
         {
-            //Create 2 forms in the database
-            controller.Create(a);
-            controller.Create(b);
-            //Verify that Add was called to the database exactly 2 times
-            mockSet.Verify(m => m.Add(It.IsAny<Form>()), Times.Exactly(2));
-            //Verify that SaveChanges is called 2 times.
-            mockContext.Verify(m => m.SaveChanges(), Times.Exactly(2));
+            
         }
         [TestMethod]
         public void TestAddingRules()
         {
-            mockContext.Setup(m => m.Forms).Returns(mockSet.Object);
-            controller.Create(a);
-            controller.Create(b);
-            controller.Create(c);
-            mockSet.Verify(m => m.Add(It.IsAny<Form>()), Times.Exactly(2));
-            mockContext.Verify(m => m.SaveChanges(), Times.Exactly(2));
+            
         }
     }
 }
